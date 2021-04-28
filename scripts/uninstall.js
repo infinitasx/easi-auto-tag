56667a07a70ce2542279ca53abd3aa6a1fb6d174
Chris Chen <me.chenying@gmail.com>
2021-04-28 12:04:07 +0800

    feat: 增加卸载脚本

---
+++ scripts/uninstall.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+
+const cwd = process.cwd()
+if (cwd.indexOf('node_modules') >= 0) {
+  const appRoot = path.normalize(cwd.slice(0, cwd.lastIndexOf('node_modules')))
+  const pkgPath = path.join(appRoot, 'package.json')
+  const pkg = JSON.parse(fs.readFileSync(pkgPath).toString('utf-8'))
+
+  // delete script
+  if (pkg.hasOwnProperty('scripts')) {
+    if (pkg.scripts.hasOwnProperty('easi-auto-tag')) {
+      delete pkg.scripts['easi-auto-tag']
+    }
+  }
+}
